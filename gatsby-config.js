c08b920fb1c19ce71880ff299926c8e30c3ceb63
nikkig124 <nikkig124@gmail.com>
2020-09-07 10:09:04 -0500

    packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,33 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
+  siteMetadata: {
+    title: "Nicole Gibson Portfolio",
+    description: "This is my portfolio to display my work as a Web Developer",
+    author: "@nikkig124",
+    data: ["item1", "item2"],
+    person: { name: "project1", year: "2019" },
+  },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        // Add any options here
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `bdhxyoqouzw8`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
